89375634cbe2b8b95544aa68a336175a7f0643c6
generaldave <icepicktoeye@gmail.com>
2017-01-17 17:45:33 -0500

    Initial commit

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
 HOST    = "irc.twitch.tv"
 PORT    = 6667
-PASS    = "oauth:678h32eswd33lm8rj409cqn189yabd"
-NICK    = "mastergunsbot"
-CHANNEL = "generaldave"
+PASS    = "oauth: gotten from https://twitchapps.com/tmi"
+NICK    = "bot name / must have its own twitch account"
+CHANNEL = "channel to connect to"
